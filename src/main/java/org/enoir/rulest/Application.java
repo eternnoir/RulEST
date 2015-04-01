12f40cf0cf56f2de088cd5b9694fbe38f87b5abe
eternnoir <eternnoir@gmail.com>
2015-04-01 10:33:57 +0800

    Add main class.

---
+++ src/main/java/org/enoir/rulest/Application.java
@@ -0,0 +1,14 @@
+package org.enoir.rulest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by frank on 2015/4/1.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
