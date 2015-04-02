89b32356d5ceb61ffb845d3813d305615ff04f85
eternnoir <eternnoir@gmail.com>
2015-04-02 14:48:07 +0800

    Add .travis

---
+++ src/main/java/org/enoir/rulest/ruleengine/drools/Resource.java
@@ -0,0 +1,7 @@
+package org.enoir.rulest.ruleengine.drools;
+
+/**
+ * Created by frank on 2015/4/2.
+ */
+public class Resource {
+}
---
+++ src/test/java/org.enoir.rulest.test/KnowledgeManagerTest.java
@@ -0,0 +1,7 @@
+package org.enoir.rulest.test;
+
+/**
+ * Created by frank on 2015/4/2.
+ */
+public class KnowledgeManagerTest {
+}
